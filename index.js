d5c4a310563a772b628e67151fe7f88ae1503691
Niro-Programe <fernandoakshu5@gmail.com>
2025-06-28 15:17:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ async function connectToWA() {;
         }).child({
             level: "fatal"
         }),
-        printQRInTerminal: true,
+        printQRInTerminal: false,
         generateHighQualityLinkPreview: true,
         auth: state,
         defaultQueryTimeoutMs: undefined,
